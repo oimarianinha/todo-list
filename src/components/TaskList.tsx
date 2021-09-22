e0dc238b7ca8e5215bf27d8671a88167e6c4096b
oimarianinha <mariananunestaborda@gmail.com>
2021-09-21 21:12:47 -0300

    Alterando mensagem para passar nos testes na plataforma da Rocket

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -61,7 +61,7 @@ export function TaskList() {
         <div className="input-group">
           <input
             type="text"
-            placeholder="Adicionar nova tarefa"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
             autoFocus
